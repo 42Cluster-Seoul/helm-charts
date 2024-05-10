bb8f708bca38683ea77ad1f68e4e084c962e7739
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-05-10 17:40:17 +0900

    ✨ feat: 선택지 무한루프 추가 (애플리케이션 생성)

--- helm-charts-generator/main.sh
+++ helm-charts-generator/main.sh
@@ -1,58 +1,115 @@
-read -p "Enter the value for application name: " app_name
 
 # Variables 
 OUTPUT_DIRECTORY="../stable"
-DESTINATION_DIRECTORY="$OUTPUT_DIRECTORY/$app_name"
-OUTPUT_CHARTS_FILE="$DESTINATION_DIRECTORY/Chart.yaml"
-OUTPUT_VALUES_FILE="$DESTINATION_DIRECTORY/values.yaml"
 HELM_TEMPLATES_DIRECTORY="template/templates"
 GITHUB_REPOSITORY="https://github.com/42Cluster-Seoul/helm-charts"
 
-if [ ! -d "$DESTINATION_DIRECTORY" ]; then
-  mkdir -p "$DESTINATION_DIRECTORY"
-else
-  rm -rf "$DESTINATION_DIRECTORY"
-  mkdir -p "$DESTINATION_DIRECTORY"
-fi
+create_application() {
+  read -p "Enter the value for application name: " app_name
 
-cp template/values.yaml.template $OUTPUT_VALUES_FILE
-cp template/Chart.yaml.template $OUTPUT_CHARTS_FILE
+  DESTINATION_DIRECTORY="$OUTPUT_DIRECTORY/$app_name"
+  OUTPUT_CHARTS_FILE="$DESTINATION_DIRECTORY/Chart.yaml"
+  OUTPUT_VALUES_FILE="$DESTINATION_DIRECTORY/values.yaml"
 
-sed -i.tmp "s/\${app_name}/$app_name/g" $OUTPUT_CHARTS_FILE
+  if [ ! -d "$DESTINATION_DIRECTORY" ]; then
+    mkdir -p "$DESTINATION_DIRECTORY"
+  else
+    while true; do
+      read -p "Application already exists. Do you want to overwrite it? (y/n): " overwrite
+      if [ "$overwrite" == "n" ]; then
+        echo "Stop creating application. Exiting program."
+        exit 0
+      elif [ "$overwrite" == "y" ]; then
+        rm -rf "$DESTINATION_DIRECTORY"
+        mkdir -p "$DESTINATION_DIRECTORY"
+        echo "Overwriting the application: $app_name"
+        break
+      else
+        echo "Invalid input. Please enter 'y' or 'n'."
+      fi
+    done
+  fi
 
-read -p "Enter replicas count: " replicas_count
-read -p "Enter image url: " image_url
-read -p "Enter port number: " port_number
+  cp template/values.yaml.template $OUTPUT_VALUES_FILE
+  cp template/Chart.yaml.template $OUTPUT_CHARTS_FILE
 
-sed -i.tmp \
-    -e "s/\${app_name}/$app_name/g" \
-    -e "s/\${replicas_count}/$replicas_count/g" \
-    -e "s/\${image_url}/$image_url/g" \
-    -e "s/\${port_number}/$port_number/g" \
-    "$OUTPUT_VALUES_FILE"
+  sed -i.tmp "s/\${app_name}/$app_name/g" $OUTPUT_CHARTS_FILE
 
-rm -f "$OUTPUT_CHARTS_FILE.tmp" "$OUTPUT_VALUES_FILE.tmp"
+  read -p "Enter replicas count: " replicas_count
+  read -p "Enter image url: " image_url
+  read -p "Enter port number: " port_number
 
-cp -r "$HELM_TEMPLATES_DIRECTORY" "$DESTINATION_DIRECTORY"
+  sed -i.tmp \
+      -e "s/\${app_name}/$app_name/g" \
+      -e "s/\${replicas_count}/$replicas_count/g" \
+      -e "s/\${image_url}/$image_url/g" \
+      -e "s/\${port_number}/$port_number/g" \
+      "$OUTPUT_VALUES_FILE"
 
-sed -i.tmp "s/\${app_name}/$app_name/g" "$DESTINATION_DIRECTORY/templates/application.yaml"
+  rm -f "$OUTPUT_CHARTS_FILE.tmp" "$OUTPUT_VALUES_FILE.tmp"
 
-rm -f "$DESTINATION_DIRECTORY/templates/application.yaml.tmp"
+  cp -r "$HELM_TEMPLATES_DIRECTORY" "$DESTINATION_DIRECTORY"
 
-mv "$DESTINATION_DIRECTORY/templates/application.yaml" "$OUTPUT_DIRECTORY/$app_name-applcation.yaml"
+  sed -i.tmp "s/\${app_name}/$app_name/g" "$DESTINATION_DIRECTORY/templates/application.yaml"
 
-echo -e "\n🪽 New Helm Charts created with app name: $app_name \n"
+  rm -f "$DESTINATION_DIRECTORY/templates/application.yaml.tmp"
 
-echo -e "✅ Lint Check for application: $app_name\n"
+  mv "$DESTINATION_DIRECTORY/templates/application.yaml" "$OUTPUT_DIRECTORY/$app_name-applcation.yaml"
 
-helm lint $DESTINATION_DIRECTORY
+  echo -e "\n🪽 New Helm Charts created with app name: $app_name \n"
 
-echo -e "⏫ Uploading Helm Chart to Github Repository: $GITHUB_REPOSITORY \n"
+  echo -e "✅ Lint Check for application: $app_name\n"
 
-git add $OUTPUT_DIRECTORY
-git commit -m "Added new application: $app_name"
-git push origin main
+  helm lint $DESTINATION_DIRECTORY
 
-echo -e "\n🚀 Applying for application: $app_name-application.yaml\n"
+  echo -e "⏫ Uploading Helm Chart to Github Repository: $GITHUB_REPOSITORY \n"
 
-kubectl create -f $OUTPUT_DIRECTORY/$app_name-applcation.yaml
+  git add $OUTPUT_DIRECTORY
+  git commit -m "Added new application: $app_name"
+  git push origin main
+
+  echo -e "\n🚀 Applying for application: $app_name-application.yaml\n"
+
+  kubectl create -f $OUTPUT_DIRECTORY/$app_name-applcation.yaml
+}
+
+main() {
+  echo "🚀 Helm Charts Generator"
+  echo "========================"
+  echo "📝 This script will help you to create Helm Charts for your application."
+  echo "📝 You can also deploy the application using the generated Helm Charts."
+
+  while true; do
+    echo "========================"
+    echo "📥 Select an option "
+    echo "1. Create Application"
+    echo "2. Delete Application"
+    echo "3. Display Currently Deployed Applications"
+    echo "4. Exit"
+    echo "========================"
+
+    read -p "✅ Enter your choice: " choice
+
+    case $choice in
+          1)
+              create_application
+              ;;
+          2)
+              echo "You chose Option 2"
+              ;;
+          3)
+              echo "You chose Option 3"
+              ;;
+          4)
+              echo "❌ Exiting program"
+              exit 0
+              ;;
+          *)
+              echo "Invalid choice. Please enter a number between 1 and 4."
+              ;;
+      esac
+
+  done
+}
+
+main
